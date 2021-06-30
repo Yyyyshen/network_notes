a98a175e655cf7e436952b4138e10d908d8b70eb
Yyyyshen <Yyyyshen@gmail.com>
2021-06-30 13:46:12 +0800

    测试百万TCP连接

---
+++ practise_million_conns/practise_million_conns.cpp
@@ -0,0 +1,18 @@
+﻿// practise_million_conns.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+
+#include <iostream>
+
+//
+//测试单机百万TCP连接
+// 参考 飞哥开发内功修炼网络部分的文章
+// https://mp.weixin.qq.com/s?__biz=MjM5Njg5NDgwNA==&mid=2247484691&idx=1&sn=6230c5bf536265ec3280008761cce0a7&scene=21#wechat_redirect
+//
+
+//
+//
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
