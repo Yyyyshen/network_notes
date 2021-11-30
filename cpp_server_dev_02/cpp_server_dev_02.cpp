f0b8b3e4126f7560a8db5464be9a6a0579698a2c
Yyyyshen <Yyyyshen@gmail.com>
2021-11-30 11:12:24 +0800

    cmake简记

--- cpp_server_dev_02/cpp_server_dev_02.cpp
+++ cpp_server_dev_02/cpp_server_dev_02.cpp
@@ -4,10 +4,24 @@
 #include <iostream>
 
 //
+//C++后端开发工具和调试知识
 //
+
+//
+//ssh
+// xshell（我win用的putty，没有可视界面但轻量，mac下用的FinalShell）
+// 
+//ftp
+// FileZilla（这个与笔者习惯一致）
+//
+
+//
+//makefile和cmake
+// 一般使用makefile组织C/C++项目
+// 而makefile不太方便，就有了cmake，可以通过CMakeLists.txt文件生成makefile文件
 //
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
 }
