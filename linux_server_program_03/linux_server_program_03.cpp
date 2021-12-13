f4109f211a9bc79aaa538fdd032f7e77a2c067dd
Yyyyshen <Yyyyshen@gmail.com>
2021-12-13 12:11:14 +0800

    socket地址

--- linux_server_program_03/linux_server_program_03.cpp
+++ linux_server_program_03/linux_server_program_03.cpp
@@ -8,10 +8,97 @@
 //
 
 //
+//Linux网络编程基础API
 //
+
+//
+//socket地址API
+// 
+//主机字节序和网络字节序
+// 32位机器CPU，累加器一次装载4字节，数据在内存中的顺序影响装载后的值
+// 分为大端和小端
+// 大端是高位在低地址，低位在高地址；小端是反过来
+void
+test_byteorder()
+{
+	union test
+	{
+		short value;
+		char union_bytes[sizeof(short)];
+	};
+	test u;
+	u.value = 0x0102;
+	if (u.union_bytes[0] == 1 && u.union_bytes[1] == 2)
+		std::cout << "big endian" << std::endl;
+	else if (u.union_bytes[1] == 1 && u.union_bytes[0] == 2)
+		std::cout << "little endian" << std::endl;
+	else
+		std::cout << "unknown..." << std::endl;
+}
+// 现代PC大多是小端字节序，因此又称为主机字节序
+// 两台不同字节序的主机直接传递数据会出错
+// 方法是，发送端总是把数据转成大端，接收端总是默认传过来的是大端，然后根据自身字节序选则是否转换
+// 所以大端又称为网络字节序
+// 同一台机器上两个进程通信，也要考虑字节序问题（例如C和JAVA，java采用大端）
+// htonl、htons、ntohl、ntohs
+// h表示host、n表示network、l和s表示long和short，长整型常用来转换IP，短整型转端口
+// 
+//通用socket地址
+// Linux下最基本socket地址
+// struct sockaddr
+// {
+//		sa_family_t sa_family;
+//		char sa_data[14];
+// }
+// sa_family是地址族变量，对应协议族变量
+// AF_UNIX		PF_UNIX		Unix本地域协议族	文件路径名，长度可达108字节
+// AF_INET		PF_INET		TCP/IPv4协议族		16bit端口和32bitIPv4地址，共6字节
+// AF_INET6		PF_INET6	TCP/IPv6协议族		16bit端口和32bit流标识，128bitIPv6地址，32bit范围ID，共26字节
+// 可见，sockaddr只能用于IPv4
+// 新的通用地址结构体
+// struct sockaddr_storage
+// {
+//		sa_family_t sa_family;
+//		unsigned long int __ss_align;
+//		char __ss_padding[128-sizeof(__ss_align)];
+// }
+// 提供了足够大的空间用于存储地址值，并且是内存对齐(__ss_align)
+// 
+//专用地址结构
+// 通用的地址结构只有数据的数组成员，需要自己把IP端口号做处理才能设置或获取
+// Linux提供专用于各个协议族的地址结构体
+// sockaddr_un、sockaddr_in、sockaddr_in6
+// 最常用的v4结构如下
+// struct sockaddr_in
+// {
+//		sa_family_t sin_family;
+//		u_int16_t sin_port;
+//		struct in_addr sin_addr;
+// }
+// 其中
+// struct in_addr
+// {
+//		u_int32_t s_addr;
+// }
+// 专用的socket地址更方便设置或获取成员值，实际使用时，设置后直接强转为sockaddr类型即可，因为接口参数都是这个类型
+// 
+//IP地址转换函数
+// 人习惯用点分制字符串，而地址结构中是整型，所以需要转换
+// inet_addr、inet_aton、inte_ntoa
+// a表示address、n表示network，相互转换
+// 也可以使用新函数兼容IPv4和v6
+// inet_pton、inet_ntop
+// 多一个参数传递地址族，还有地址族长度大小，涉及到两个辅助宏
+// INET_ADDRSTRLEN 16
+// INET_ADDRESTRLEN 46
+//
+
+//
+//创建socket
+// 
 //
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	test_byteorder();
 }
